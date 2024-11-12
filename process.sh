d9eab56ead08aac49aacc0a39669b1ff8b8e14aa
kmraf <48165221+kmraf@users.noreply.github.com>
2024-11-12 16:01:19 +0300

    Update process.sh

--- process.sh
+++ process.sh
@@ -2,6 +2,6 @@ cat $1 | awk -f script1.awk
 
 cat $1 | awk -F , 'NF>0{print tolower($7),$2}' | sort | uniq | awk '{print $1}' | uniq -c | awk '{print "HOTELNUMBER", $2, $1}'
 
-cat $1 | grep -Ei "(holiday inn|hilton)" | awk -F , -f script2.awk
+cat $1 | grep -Ei "holiday inn|hilton" | awk -F , -f script2.awk
 
 gnuplot script.p
